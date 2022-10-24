08576b81b3d2b53a7e31fbbfe184fb50af79a26a
w0748346 <zhinkee@gmail.com>
2022-10-24 15:08:52 -0500

    filereader and filewriter

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,18 @@
     -read stuff from a text file
     -write stuff to a file
 
-*
-*
-*
-*
-*
-*
-*
-*
-*
  */
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         FileReader fr = new FileReader(
                 "C:\\Users\\w0748346\\Desktop\\Random.txt");     //expects arguements (string) file location + filename
 
+        //READ STuFF FROM TEXT FILE
 /*
         char c = (char)fr.read();
         System.out.println(c);          //read char itself
@@ -35,6 +28,7 @@ public class Main {
         System.out.println(i);     // when there is no more characters, will read -1
 */
 
+/*
         //create a while loop until you get -1 to read all the available characters
         int num = fr.read();
         while (num != -1) {
@@ -42,6 +36,32 @@ public class Main {
             System.out.println(c);
             num = fr.read();
         }
+*/
+/*
+        //gets total number of new lines
+        int num = fr.read();
+        int totalNewLine= 1;
+        while (num != -1) {
+            if (num == 10)
+                totalNewLine++;
+                num = fr.read();
+        }
+        System.out.println(totalNewLine);
+*/
+
+        //WRITE STUFF TO A FILE!!
+
+        FileWriter fw = new FileWriter("C:\\Users\\w0748346\\Desktop\\bread.txt");
+        // if we overload Filewriter constructor with (location, *true*) everytime you run the program, it'll add and not replace. Default is false
+
+        fw.write('B');
+        fw.write('R');
+        fw.write('E');
+        fw.write('A');
+        fw.write('D');
+        fw.write(" is cool!");                  //can write several characters at the same time
+        fw.close();                                 //this line does the actual writing of the file
+
 
     }
 
